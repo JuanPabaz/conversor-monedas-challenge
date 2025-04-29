d666c1ce3d35de51ae7b74cb375956ee36bf6182
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-04-29 11:08:09 -0500

    feat: update the cases when converting the currencies

--- src/com/jpproject/currencyconversor/calculations/MoneyConversion.java
+++ src/com/jpproject/currencyconversor/calculations/MoneyConversion.java
@@ -3,14 +3,12 @@ package com.jpproject.currencyconversor.calculations;
 import com.jpproject.currencyconversor.models.ExchangeRateResponse;
 
 public class MoneyConversion {
-    public static Double convertCurrencies(int opcion, ExchangeRateResponse exchangeRateResponse) {
+    public static Double convertCurrencies(int opcion, ExchangeRateResponse exchangeRateResponse, double value) {
         return switch (opcion) {
-            case 1 -> exchangeRateResponse.usd() / exchangeRateResponse.ars();
-            case 2 -> exchangeRateResponse.ars() * exchangeRateResponse.usd();
-            case 3 -> exchangeRateResponse.usd() / exchangeRateResponse.brl();
-            case 4 -> exchangeRateResponse.usd() * exchangeRateResponse.brl();
-            case 5 -> exchangeRateResponse.usd() / exchangeRateResponse.cop();
-            case 6 -> exchangeRateResponse.usd() * exchangeRateResponse.cop();
+            case 1-> value * exchangeRateResponse.conversionRates().get("ARS");
+            case 2,4,6 -> value * exchangeRateResponse.conversionRates().get("USD");
+            case 3 -> value * exchangeRateResponse.conversionRates().get("BRL");
+            case 5 -> value * exchangeRateResponse.conversionRates().get("COP");
             default -> null;
         };
     }
